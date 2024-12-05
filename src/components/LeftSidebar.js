a71a15a4bfa21438038dddd0586524466c2631ea
shivani <shivanitripathi.etlhive@gmail.com>
2024-12-05 13:17:57 +0530

    nine repo

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -5,37 +5,52 @@ import { IoIosNotificationsOutline } from "react-icons/io";
 import { CiUser } from "react-icons/ci";
 import { CiBookmark } from "react-icons/ci";
 import { AiOutlineLogout } from "react-icons/ai";
-import { Link,useNavigate } from 'react-router-dom';
-import {useSelector,useDispatch} from "react-redux";
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import { USER_API_END_POINT } from '../utils/constant';
-import toast from "react-hot-toast"
+import toast from "react-hot-toast";
 import { getMyProfile, getOtherUsers, getUser } from '../redux/userSlice';
- 
 
 const LeftSidebar = () => {
-
-    const {user} = useSelector(store=>store.user);
+    const { user } = useSelector(store => store.user);
     const navigate = useNavigate();
     const dispatch = useDispatch();
+
     const logoutHandler = async () => {
         try {
+            // Clear the token from localStorage
+            localStorage.removeItem('authToken');
+
+            // Call the backend logout endpoint
             const res = await axios.get(`${USER_API_END_POINT}/logout`);
+
+            // Clear user-related Redux states
             dispatch(getUser(null));
             dispatch(getOtherUsers(null));
             dispatch(getMyProfile(null));
+
+            // Navigate to login page
             navigate('/login');
+
+            // Show success toast message
             toast.success(res.data.message);
         } catch (error) {
-            console.log(error);
+            console.error("Logout failed:", error);
+            toast.error(error.response?.data?.message || "Something went wrong!");
         }
-    }
+    };
 
     return (
         <div className='w-[20%]'>
             <div>
                 <div>
-                    <img className='ml-5' width={"24px"} src="https://www.edigitalagency.com.au/wp-content/uploads/new-Twitter-logo-x-black-png-1200x1227.png" alt="twitter-logo" />
+                    <img
+                        className='ml-5'
+                        width={"24px"}
+                        src="https://www.edigitalagency.com.au/wp-content/uploads/new-Twitter-logo-x-black-png-1200x1227.png"
+                        alt="twitter-logo"
+                    />
                 </div>
                 <div className='my-4'>
                     <Link to="/" className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full'>
@@ -74,11 +89,13 @@ const LeftSidebar = () => {
                         </div>
                         <h1 className='font-bold text-lg ml-2'>Logout</h1>
                     </div>
-                     <button className='px-4 py-2 border-none text-md bg-[#1D9BF0] w-full rounded-full text-white font-bold'>Post</button>
+                    <button className='px-4 py-2 border-none text-md bg-[#1D9BF0] w-full rounded-full text-white font-bold'>
+                        Post
+                    </button>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
